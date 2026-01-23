7c27d2cac57485099098550287578f18989d87c2
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2026-01-23 10:35:08 +0530

    added gitignore

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
 
-app.use((req,res)=>{
-    res.send("Hello server")
-})
 app.use("/test",(req,res)=>{
     res.send("Testing...")
 })
+
+app.use((req,res)=>{
+    res.send("Hello server")
+})
+
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000")
 })

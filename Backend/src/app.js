b8dca8965b0083404883c8bd05d14a47afec671b
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2026-03-24 10:07:33 +0530

    connected to database

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,37 @@
 const express=require('express');
 const app=express();
+const connectDB=require("./config/database")
+const User=require("./models/user")
+app.use(express.json())
+require('dotenv').config();
 
-app.use("/test",(req,res)=>{
-    res.send("Testing...")
+app.post("/signup",async(req,res)=>{
+    console.log(req.body)
+    const userObj={
+        firstName:"Hadifa",
+        lastName:"Rousheen",
+        email:"hadifarousheen2644@gmail.com"
+    }
+    const user= new User(req.body)
+    try{
+        await user.save();
+        res.send("User Added")
+    }catch(err){
+        console.log("error")
+    }
+
 })
 
-app.use((req,res)=>{
-    res.send("Hello server")
-})
+   
 
+
+
+
+
+connectDB().then(()=>{
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000")
+})
+}).catch(()=>{
+    console.log("Cannot connect to database")
 })
